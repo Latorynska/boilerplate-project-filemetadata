06d5a7db17c3130acfedfad413cb5b2cd7fc1bbc
Latorynska <paksiringkang225@gmail.com>
2025-09-26 11:15:16 +0700

    done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 require('dotenv').config()
+let bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,6 +16,17 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
